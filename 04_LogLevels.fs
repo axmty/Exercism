242ff224b742fd66c8c06c75f1b243f7c2acde37
Alexandre Metgy <alexandre.metgy@gmail.com>
2022-11-08 23:23:32 +0100

    Exercise

---
+++ 04_LogLevels.fs
@@ -0,0 +1,13 @@
+﻿module LogLevels
+
+let message (logLine: string) : string =
+    let startIndex = logLine.IndexOf(':') + 1
+    logLine[ startIndex.. ].Trim()
+
+let logLevel (logLine: string) : string =
+    let startIndex = logLine.IndexOf('[') + 1
+    let endIndex = logLine.IndexOf(']') - 1
+    logLine[ startIndex..endIndex ].ToLower()
+
+let reformat (logLine: string) : string =
+    $"{message logLine} ({logLevel logLine})"
