f4aeaf929438ea27665a80a378f09f02d43da536
Alexandre Metgy <alexandre.metgy@gmail.com>
2022-11-08 23:03:35 +0100

    Exercise

---
+++ 02_LuciansLusciousLasagna.fs
@@ -0,0 +1,12 @@
+﻿module LuciansLusciousLasagna
+
+let expectedMinutesInOven = 40
+
+let remainingMinutesInOven actualMinutesInOven =
+    expectedMinutesInOven - actualMinutesInOven
+
+let preparationTimeInMinutes numberOfLayers = 2 * numberOfLayers
+
+let elapsedTimeInMinutes numberOfLayers actualMinutesInOven =
+    preparationTimeInMinutes numberOfLayers
+    + actualMinutesInOven
